0f7fbc566ced92b7d189ce8826fc7613318ff82e
cengleby86 <cengleby86@gmail.com>
2023-01-16 23:56:55 -0500

    checking

--- website/source/handler.py
+++ website/source/handler.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.gzip import GZipMiddleware
+import requests
+from datetime import datetime
 # from models import Pages, Database
 
 app = FastAPI()
@@ -13,9 +15,39 @@ templates = Jinja2Templates(directory=["views"])
 # ROOT
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    data = {
-        "page": "Home page"
-    }
+
+    page = 1
+    jobs = []
+    while True:
+        results = requests.get(
+            headers={
+                "PRIVATE-TOKEN": "token-string-here123"
+            },
+            url=f"http://repo.dev.hrl.internal/api/v4/projects/2/jobs?page={page}&per_page=100"
+        )
+        jobs = jobs + results.json()
+
+        if len(results.json()) < 100:
+            break
+            
+        page += 1
+
+    data = []
+    for job in jobs:
+        if len(job['artifacts']) > 1:
+            for artifact in job['artifacts']:
+                if "artifact" in artifact['filename']:
+                    data.append({
+                        "job_id": job['id'],
+                        "created_at": datetime.strptime(
+                            job['created_at'], 
+                            "%Y-%m-%dT%H:%M:%S.%fZ"
+                        ).strftime("%B-%d-%Y %H:%M %p"),
+                        "actor": job['commit']['author_name'],
+                        "file_size": job['artifacts_file']['size'],
+                        "file_name": job['artifacts_file']['filename']
+                    })
+
     return templates.TemplateResponse("index.jinja", {"request": request, "data": data})
 
 if __name__ == "__main__":
